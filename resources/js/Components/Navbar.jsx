79f80eb3bb36c1f287008d91b7ffa28c3408a069
ervalsa <ervalsa-san@gmail.com>
2022-10-14 03:46:15 +0800

    Rename navbar item

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -9,19 +9,28 @@ export default function Navbar() {
         <div>
             <nav className="bg-white border-b border-gray-100 h-12 flex items-center">
                 <div className="container">
-                    <div className="flex items-center gap-x-4">
-                        <Link href={route('threads.index')}>Threads</Link>
+                    <div className="flex justify-between items-center gap-x-4">
+                        <div>
+                            <Link href={route('home')}>ETAMCODE</Link>
+                        </div>
+                        <div className="flex items-center gap-x-4">
+                            <Link className="hover:bg-" href={route('home')}>Home</Link>
+                            <Link href={route('threads.index')}>Forum Q&A</Link>
+                            <Link href={'#'}>Blog</Link>
+                            <Link href={'#'}>FAQ</Link>
+                        </div>
                         {auth.user ?
                             <div className="flex items-center gap-x-4">
                                 <Link href={route('threads.create')}>New Thread</Link>
+                                <Link href={route('dashboard')}>Dashboard</Link>
                                 <Link method="post" href={route('logout')} as="button">
                                     Log Out
                                 </Link>
                             </div>
                             :
                             <div className="flex items-center gap-x-4">
-                                <Link href="/login">Login</Link>
-                                <Link href="/register">Register</Link>
+                                <Link href="/login">Masuk</Link>
+                                <Link href="/register">Daftar</Link>
                             </div>
                         }
                     </div>
